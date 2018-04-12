ff7932c884fd0c1e334b63601179760c137e9ec1
marc.labie <marc.labie@heig-vd.ch>
2018-04-13 00:25:01 +0200

    Init Repo

---
+++ SMTPProtocolClient-code/src/main/java/ch/heigvd/res/labs/smtp/Amorce.java
@@ -0,0 +1,7 @@
+package ch.heigvd.res.labs.smtp;
+
+public class Amorce {
+    public static void main(String args[]){
+        System.out.println("hello world");
+    }
+}
---
+++ SMTPProtocolClient-code/src/main/java/ch/heigvd/res/labs/smtp/net/protocol/SMTPProtocol.java
@@ -0,0 +1,10 @@
+package ch.heigvd.res.labs.smtp.net.protocol;
+
+/**
+ *
+ * @author David Jaquet & Marc Labie
+ */
+public class SMTPProtocol {
+    public final static String CMD_CLEAR = "CLEAR";
+    public final static String CMD_LIST = "LIST";
+}
